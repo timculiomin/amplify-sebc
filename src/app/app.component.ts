4d1656c97124962bb8206b75d2b7b1bc659889a9
Tim Culiomin <cultim000@gmail.com>
2025-03-22 23:31:08 -0600

    hide based off permission

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TodosComponent } from './todos/todos.component';
 import { Amplify } from 'aws-amplify';
@@ -6,6 +6,7 @@ import outputs from '../../amplify_outputs.json';
 import { AmplifyAuthenticatorModule, AuthenticatorService } from '@aws-amplify/ui-angular';
 import { FileUploadComponent } from './file-upload/file-upload.component';
 import { FileDisplayComponent } from './file-display/file-display.component';
+import { AuthTokens, AuthSession, AuthUser, fetchAuthSession } from 'aws-amplify/auth';
 
 @Component({
   selector: 'app-root',
@@ -14,9 +15,22 @@ import { FileDisplayComponent } from './file-display/file-display.component';
   styleUrl: './app.component.css',
   imports: [RouterOutlet, TodosComponent, AmplifyAuthenticatorModule, FileUploadComponent, FileDisplayComponent],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'amplify-angular-template';
+  isUploaderUser = signal(false);
 
   constructor(public authenticator: AuthenticatorService) {
+  }
+
+  async ngOnInit() {
+    try {
+      const session = await fetchAuthSession();
+      const rawGroups = session.tokens?.accessToken.payload?.['cognito:groups'];
+      const groups = Array.isArray(rawGroups) ? rawGroups as string[] : [];
+      this.isUploaderUser.set(groups.includes('Uploader'));
+      console.log('isUploaderUser', this.isUploaderUser());
+    } catch (err) {
+      console.error('Error getting user session:', err);
+    }
   }
 }
